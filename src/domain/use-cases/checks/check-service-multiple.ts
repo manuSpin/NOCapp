45501f1ccbfc5d18055481a1f065860f0c112f77
Manuel Fernández <manuel.ferca.91@gmail.com>
2025-02-27 08:16:55 +0100

    Servicio multiple para guardar los logs

---
+++ src/domain/use-cases/checks/check-service-multiple.ts
@@ -0,0 +1,48 @@
+import { LogEntity, LogSeverityLevel } from "../../entities";
+import { LogRepository } from "../../repository/log.repository";
+
+interface CheckServiceMultipleUseCase {
+    execute(url: string): Promise<boolean>;
+}
+
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
+export class CheckServiceMultiple implements CheckServiceMultipleUseCase {
+    private static filename: string = 'check-service.ts';
+
+    constructor(
+        private readonly logRepository: LogRepository[],
+        private readonly successCallBack: SuccessCallback,
+        private readonly errorCallback: ErrorCallback
+    ) { }
+
+    private createLog(message: string, severityLevel: LogSeverityLevel): void {
+        const log = new LogEntity({ message: message, level: severityLevel, origin: CheckServiceMultiple.filename });
+
+        this.logRepository.forEach(logRepository => {
+            logRepository.saveLog(log);
+        });
+    }
+
+    public async execute(url: string): Promise<boolean> {
+        try {
+            const requ = await fetch(url);
+            if (!requ.ok) {
+                const errorMessage = `Error on check service ${url}`;
+                this.createLog(errorMessage, LogSeverityLevel.medium);
+                throw new Error(errorMessage);
+            }
+
+            this.createLog(`Service ${url} working`, LogSeverityLevel.low);
+            this.successCallBack();
+            return true;
+
+        } catch (error) {
+            const messageError = `${url} is not ok. Reason: ${error}`;
+            this.createLog(messageError, LogSeverityLevel.high);
+            this.errorCallback(messageError);
+            return false;
+        }
+    }
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,15 @@
 import { LogSeverityLevel } from "../domain/entities";
 import { LogRepository } from "../domain/repository/log.repository";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { FileSystemDataSource } from "../infrastructure/datasource/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasource/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-// const fileSystemRepository = new LogRepositoryImpl(new FileSystemDataSource()); // En carpeta del proyecto
-const logRepository = new LogRepositoryImpl(new MongoLogDatasource()); // En base de datos mongo
+const fileSystemRepository = new LogRepositoryImpl(new FileSystemDataSource()); // En carpeta del proyecto
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource()); // En base de datos mongo
 const emailService = new EmailService();
 
 export class Server {
@@ -20,12 +21,23 @@ export class Server {
         //     // const url = 'http://localhost:3000';
         //     const url = 'http://www.google.es';
         //     new CheckService(
-        //         logRepository,
+        //         mongoLogRepository,
         //         () => console.log(`${url} is ok`),
         //         (error) => console.log(error)
         //     ).execute(url);
         //     // new CheckService().execute('http://localhost:3000');
         // });
+
+        CronService.createJob('*/5 * * * * *', () => {
+            // const url = 'http://localhost:3000';
+            const url = 'http://www.google.es';
+            new CheckServiceMultiple(
+                [mongoLogRepository, fileSystemRepository],
+                () => console.log(`${url} is ok`),
+                (error) => console.log(error)
+            ).execute(url);
+            // new CheckService().execute('http://localhost:3000');
+        });
 
     }
 }
